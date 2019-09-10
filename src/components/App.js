2ed8c8b53fabaf10a7b351291a6727bd99f8b97e
huyle0110 <lehuy.paul@gmail.com>
2019-09-10 17:40:07 +0700

    Init Menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../logo.svg';
-import './App.css';
+import './App.scss';
 import './HomeHeader';
 import HomeHeader from './HomeHeader';
 import FooterHeader from './HomeFooter';
--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import './../assets/scss/header.scss';
 
 class HomeHeader extends Component {
     constructor(props){
         super(props);
-
     }
+    // var headlineStyle = {
 
+    // };
+    
     render() {
         return (
-            <div className="row">
-                <div className="col-sm-5">a</div>
+            <div className="row" style={{background: '#00aeef'}}>
+                <div className="col-sm-5">
+                    <i className=""></i>
+                    <h5>Hotline</h5>
+                </div>
                 <div className="col-sm-7">b</div>
             </div>
         );
